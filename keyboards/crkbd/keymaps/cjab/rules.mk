0a2b116a7a12e3a982af08fbcf2762d89c216e9b
Chad Jablonski <chad@jablonski.xyz>
2025-01-10 23:21:03 -0500

    Asym eager debounce

--- keyboards/crkbd/keymaps/cjab/rules.mk
+++ keyboards/crkbd/keymaps/cjab/rules.mk
@@ -3,8 +3,8 @@ MOUSEKEY_ENABLE  = no
 RGBLIGHT_ENABLE  = no
 OLED_ENABLE      = no
 LTO_ENABLE       = yes
-DEBOUNCE_TYPE    = sym_defer_pk
-DEBOUNCE         = 5
+DEBOUNCE_TYPE    = asym_eager_defer_pk
+DEBOUNCE         = 15
 
 CONVERT_TO       = elite_pi
 SERIAL_DRIVER    = vendor
